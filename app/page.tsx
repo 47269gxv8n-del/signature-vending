c6f3cb3103e31514ba0db73b1f45b11fbac74884
Paul Downes <your@email.com>
2026-01-07 15:46:14 +0000

    same

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
   <img
   src="/Lobbypic.png"
   alt="Signature Vending"
-className="h-[20rem] md:h-[20rem] w-[60rem]mb-12"
+className="h-[20rem] md:h-auto w-[60rem] mb-12"
 
 />
 
